b6236ac9c348e5964b9429dac08f1f4d14e41b89
Mohammed Javed <javedamaan21@hotmail.com>
2023-09-23 10:22:15 +0100

    Build Errors fix

--- Smartwyre.DeveloperTest.Tests/AmountPerUomCalculatorTests.cs
+++ Smartwyre.DeveloperTest.Tests/AmountPerUomCalculatorTests.cs
@@ -3,79 +3,77 @@ using Smartwyre.DeveloperTest.Services;
 using Smartwyre.DeveloperTest.Types;
 using Xunit;
 
-namespace Smartwyre.DeveloperTest.Tests
+namespace Smartwyre.DeveloperTest.Tests;
+
+public class AmountPerUomCalculatorTests
 {
+    private readonly AmountPerUomCalculator _amountPerUomCalc = new();
 
-    public class AmountPerUomCalculatorTests
+    [Fact]
+    public void UnsupportedTypeReturnsFalseForProduct()
     {
-        private readonly AmountPerUomCalculator _amountPerUomCalc = new();
-
-        [Fact]
-        public void UnsupportedTypeReturnsFalseForProduct()
+        var product = new Product()
         {
-            var product = new Product()
-            {
-                SupportedIncentives = SupportedIncentiveType.FixedCashAmount
-            };
+            SupportedIncentives = SupportedIncentiveType.FixedCashAmount
+        };
 
-            var isSupported = _amountPerUomCalc.IncentiveTypeSupported(product);
+        var isSupported = _amountPerUomCalc.IncentiveTypeSupported(product);
 
-            isSupported.Should().BeFalse();
-        }
+        isSupported.Should().BeFalse();
+    }
 
-        [Fact]
-        public void SupportedTypeReturnsTrueForProduct()
+    [Fact]
+    public void SupportedTypeReturnsTrueForProduct()
+    {
+        var product = new Product()
         {
-            var product = new Product()
-            {
-                SupportedIncentives = SupportedIncentiveType.AmountPerUom
-            };
+            SupportedIncentives = SupportedIncentiveType.AmountPerUom
+        };
 
-            var isSupported = _amountPerUomCalc.IncentiveTypeSupported(product);
+        var isSupported = _amountPerUomCalc.IncentiveTypeSupported(product);
 
-            isSupported.Should().BeTrue();
-        }
+        isSupported.Should().BeTrue();
+    }
 
-        [Fact]
-        public void InvalidRebateReturnsInvalid()
+    [Fact]
+    public void InvalidRebateReturnsInvalid()
+    {
+        var rebate = new Rebate();
+        var product = new Product();
+        var amount = 1m;
+
+        var isValid = _amountPerUomCalc.RebateIsValid(rebate, product, amount);
+
+        isValid.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ValidRebateReturnsValid()
+    {
+        var rebate = new Rebate()
         {
-            var rebate = new Rebate();
-            var product = new Product();
-            var amount = 1m;
+            Amount = 1m
+        };
+        var product = new Product();
+        var amount = 1m;
 
-            var isValid = _amountPerUomCalc.RebateIsValid(rebate, product, amount);
+        var isValid = _amountPerUomCalc.RebateIsValid(rebate, product, amount);
 
-            isValid.Should().BeFalse();
-        }
+        isValid.Should().BeTrue();
+    }
 
-        [Fact]
-        public void ValidRebateReturnsValid()
+    [Fact]
+    public void CalculateReturnsAmount()
+    {
+        var rebate = new Rebate()
         {
-            var rebate = new Rebate()
-            {
-                Amount = 1m
-            };
-            var product = new Product();
-            var amount = 1m;
+            Amount = 3m
+        };
+        var product = new Product();
+        var amount = 2m;
 
-            var isValid = _amountPerUomCalc.RebateIsValid(rebate, product, amount);
+        var isValid = _amountPerUomCalc.CalculateAmount(rebate, product, amount);
 
-            isValid.Should().BeTrue();
-        }
-
-        [Fact]
-        public void CalculateReturnsAmount()
-        {
-            var rebate = new Rebate()
-            {
-                Amount = 3m
-            };
-            var product = new Product();
-            var amount = 2m;
-
-            var isValid = _amountPerUomCalc.CalculateAmount(rebate, product, amount);
-
-            isValid.Should().Be(6m);
-        }
+        isValid.Should().Be(6m);
     }
 }
